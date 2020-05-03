3259c6518dd930510f6d92c31653492b951f57bb
Gabriel Souza <gabrie.ssouza@gmail.com>
2020-05-02 23:24:43 -0300

    fix(build): resolve mailgun key in runtime

--- packages/server/src/graphql/modules/landing/utils.ts
+++ packages/server/src/graphql/modules/landing/utils.ts
@@ -2,9 +2,12 @@ import mailgun from 'mailgun-js';
 
 import { MAILGUN_API_KEY, MAILGUN_DOMAIN } from '../../../common';
 
-const mg = mailgun({ apiKey: MAILGUN_API_KEY, domain: MAILGUN_DOMAIN });
+console.log(MAILGUN_API_KEY);
 
-export const sendEmail = ({ from = '', to, subject, text }) =>
-  new Promise((resolve, reject) => {
+export const sendEmail = ({ from = '', to, subject, text }) => {
+  const mg = mailgun({ apiKey: MAILGUN_API_KEY, domain: MAILGUN_DOMAIN });
+  return new Promise((resolve, reject) => {
     mg.messages().send({ from, to, subject, text }, (error, body) => (error ? reject(error) : resolve(body)));
   });
+}
+
